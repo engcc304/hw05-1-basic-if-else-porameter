1fcc3051c9ab12af3f07a44a11a8278077babea4
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-14 22:36:19 +0700

    wdfwfwf

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -13,14 +13,14 @@
 */
 #include <stdio.h>
 int main() {
-    char value[4] ; //collect 4 letters.
+    char values[4] ; //collect 4 letters.
     
     for (int i = 0; i < 4; i++) { //It loops 4 times to get the character from the user and store it in value.
-        scanf( "%c", &value[i]) ; //get the value to store in values variable.
+        scanf( "%c", &values[i]) ; //get the value to store in values variable.
     }// end for
     printf( "Result: ") ; //output result on display
     for (int i = 3; i >= 0; i--) { //Loops 4 times to return the values ​​character from the end to the end.
-        printf("%c ", value[i]) ; //output on display
+        printf("%c ", values[i]) ; //output on display
     }//end for
     
     return 0 ;
