44f738097b1fe4ea118c1fd84310b98a60bb1d7c
porameter <paramet_su66@live.rmutl.ac.th>
2023-08-14 22:38:37 +0700

    ewfewfewfewfewfewfw

--- Homework/HW05/hw05-1-if-else.cpp
+++ Homework/HW05/hw05-1-if-else.cpp
@@ -11,17 +11,38 @@
     Output:
         Result: D C B A
 */
+//#include <stdio.h>
+//int main() {
+   // char values[4] ; //collect 4 letters.
+    
+   // for (int i = 0; i < 4; i++) { //It loops 4 times to get the character from the user and store it in value.
+       // scanf( "%c", &values[i]) ; //get the value to store in values variable.
+   // }// end for
+   // printf( "Result: ") ; //output result on display
+    //for (int i = 3; i >= 0; i--) { //Loops 4 times to return the values ​​character from the end to the end.
+        //printf("%c ", values[i]) ; //output on display
+    //}//end for
+    
+    //return 0 ;
+//}//end main fuction
+
+
 #include <stdio.h>
+
 int main() {
-    char values[4] ; //collect 4 letters.
-    
-    for (int i = 0; i < 4; i++) { //It loops 4 times to get the character from the user and store it in value.
-        scanf( "%c", &values[i]) ; //get the value to store in values variable.
-    }// end for
-    printf( "Result: ") ; //output result on display
-    for (int i = 3; i >= 0; i--) { //Loops 4 times to return the values ​​character from the end to the end.
-        printf("%c ", values[i]) ; //output on display
-    }//end for
-    
-    return 0 ;
-}//end main fuction
+    char values[4];
+
+    // รับอักขระ 4 ค่าจากผู้ใช้
+    printf("Enter 4 characters: ");
+    for (int i = 0; i < 4; i++) {
+        scanf(" %c", &values[i]);
+    }
+
+    // แสดงผลอักขระในลำดับกลับหลัง
+    printf("Result: ");
+    for (int i = 3; i >= 0; i--) {
+        printf("%c ", values[i]);
+    }
+
+    return 0;
+}
